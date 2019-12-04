a1f4f65c2c45805e26a13f2cb0105d2b4865ba5b
Manuel Mayer <manuel.mayer@acamti.ca>
2019-12-04 11:30:29 -0500

    Apply extensions + tests

---
+++ src/Be.Fluent/With/IEnumerables/ApplyExensions.cs
@@ -0,0 +1,35 @@
+﻿using Acamti.Be.Fluent.With.IEnumerables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Acamti.Be.Fluent.With.IEnumerables
+{
+    public static class ApplyExensions
+    {
+        public static IEnumerable<TSource> Apply<TSource>(this IEnumerable<TSource> source, Func<TSource, TSource> action)
+        {
+            var newList = Enumerable.Empty<TSource>();
+
+            foreach (var item in source.Safe())
+            {
+                newList = newList.Add(action.Invoke(item));
+            }
+
+            return newList;
+        }
+
+        public static async Task<IEnumerable<TSource>> Apply<TSource>(this IEnumerable<TSource> source, Func<TSource, Task<TSource>> action)
+        {
+            var newList = Enumerable.Empty<TSource>();
+
+            foreach (var item in source.Safe())
+            {
+                newList = newList.Add(await action.Invoke(item));
+            }
+
+            return newList;
+        }
+    }
+}
