e88989b2c00bb2a10507a5b9e4368f3c924915b1
Manuel Mayer <manuel.mayer@acamti.ca>
2019-12-04 16:04:37 -0500

    WaitAndForEach Extensions  + Tests

---
+++ src/Be.Fluent.Tests/Describe_With_Tasks/Describe_ForEachExtensions.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Acamti.Be.Fluent.With.Tasks;
+using FluentAssertions;
+using System.Threading.Tasks;
+
+namespace Be.Fluent.Tests.Describe_With_ITasks
+{
+    [TestClass]
+    public class Describe_WaitAndForEachExtensions
+    {
+        [TestMethod]
+        public async Task It_Should_ProduceDesireAction_WhenActionIsSync()
+        {
+            IEnumerable<bool> expected = new[] { false, true, false } ;
+            Task<IEnumerable<int>> initialList = Task.FromResult(new[] { 1, 2, 3 } as IEnumerable<int>);
+            List<bool> container = new List<bool>();
+
+            await initialList.WaitAndForEach(item=> container.Add(item == 2));
+
+            container.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public async Task It_Should_ProduceDesireAction_WhenSourceIsSync_AndActionIsASync()
+        {
+            IEnumerable<bool> expected = new[] { false, true, false };
+            Task<IEnumerable<int>> initialList = Task.FromResult(new[] { 1, 2, 3 } as IEnumerable<int>);
+            List<bool> container = new List<bool>();
+
+            await initialList.WaitAndForEach(async item => await Task.Run(() => container.Add(item == 2)));
+
+            container.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/Be.Fluent/With/Tasks/WaitAndForEachExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Acamti.Be.Fluent.With.IEnumerables;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Acamti.Be.Fluent.With.Tasks
+{
+    public static class WaitAndForEachExtensions
+    {
+        public static async Task WaitAndForEach<TSource>(this Task<IEnumerable<TSource>> source, Action<TSource> action)
+        {
+            (await source).ForEach(action);
+        }
+
+        public static async Task WaitAndForEach<TSource>(this Task<IEnumerable<TSource>> source, Func<TSource, Task> action)
+        {
+            await (await source).ForEach(action);
+        }
+    }
+}
