ec52ce700f71c4821623e972469a0a4c1cd4c741
Manuel Mayer <manuel.mayer@acamti.ca>
2019-12-04 10:20:36 -0500

    Safe Extensions + tests

---
+++ src/Be.Fluent.Tests/Describe_With_IEnumerables/Describe_SafeExtensions.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Acamti.Be.Fluent.With.IEnumerables;
+using FluentAssertions;
+using System.Linq;
+
+namespace Be.Fluent.Tests
+{
+    [TestClass]
+    public class Describe_SafeExtensions
+    {
+        [TestMethod]
+        public void It_Should_ReturnSameIEnumerable_WhenAlreadyInitialized()
+        {
+            IEnumerable<string> expected = new[] { "someItem" };
+
+            var initialList = new[] { "someItem" };
+
+            var result = initialList.Safe();
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void It_Should_ReturnIEnumerable_WhenNotAlreadyInitialized()
+        {
+            IEnumerable<string> expected = Enumerable.Empty<string>();
+
+            IEnumerable<string> initialList = null;
+
+            var result = initialList.Safe();
+
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/Be.Fluent/With/IEnumerables/SafeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Acamti.Be.Fluent.With.IEnumerables
+{
+    public static class SafeExtensions
+    {
+        public static IEnumerable<TSource> Safe<TSource>(this IEnumerable<TSource> source)
+        {
+            return source ?? Enumerable.Empty<TSource>();
+        }
+    }
+}
