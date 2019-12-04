8b80d4ef832c2a9894c78ab9d885b19849a4f2f2
Manuel Mayer <manuel.mayer@acamti.ca>
2019-12-04 10:16:14 -0500

    'Add' Extension + tests

---
+++ src/Be.Fluent.Tests/Describe_With_IEnumerables/Describe_AddExtensions.cs
@@ -0,0 +1,84 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using Acamti.Be.Fluent.With.IEnumerables;
+using FluentAssertions;
+
+namespace Be.Fluent.Tests
+{
+    [TestClass]
+    public class Describe_AddExtensions
+    {
+        [TestMethod]
+        public void It_Should_ReturnIEnumerable_WithAddedItem()
+        {
+            IEnumerable<string> expected = new[] { "newItem" };
+
+            var initialList = Enumerable.Empty<string>();
+
+            var result = initialList.Add("newItem");
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void It_Should_ReturnIEnumerable_WithAddedItem_WhenNotEmpty()
+        {
+            IEnumerable<string> expected = new[] { "oldItem", "newItem" };
+
+            var initialList = new[] { "oldItem" };
+
+            var result = initialList.Add("newItem");
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void It_Should_ReturnIEnumerable_WithAddedItemFromPredicate()
+        {
+            IEnumerable<string> expected = new[] { "newItem" };
+
+            var initialList = Enumerable.Empty<string>();
+
+            var result = initialList.Add(() => "newItem");
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void It_Should_ReturnIEnumerable_WithAddedItemFromPredicate_WhenNotEmpty()
+        {
+            IEnumerable<string> expected = new[] { "oldItem", "newItem" };
+
+            var initialList = new[] { "oldItem" };
+
+            var result = initialList.Add(() => "newItem");
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void It_Should_ReturnIEnumerable_WithAddedItem_WhenSourceIsNull()
+        {
+            IEnumerable<string> expected = new[] { "newItem" };
+
+            IEnumerable<string> initialList = null;
+
+            var result = initialList.Add("newItem");
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void It_Should_ReturnIEnumerable_WithAddedItemFromPredicate_WhenSourceIsNull()
+        {
+            IEnumerable<string> expected = new[] { "newItem" };
+
+            IEnumerable<string> initialList = null;
+
+            var result = initialList.Add(() => "newItem");
+
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/Be.Fluent/With/IEnumerables/AddExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Acamti.Be.Fluent.With.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Acamti.Be.Fluent.With.IEnumerables
+{
+    public static class AddExtensions
+    {
+        public static IEnumerable<TTSource> Add<TTSource>(this IEnumerable<TTSource> source, TTSource item)
+        {
+            return source
+                .Safe()
+                .Concat(item.ToEnumerable());
+        }
+
+        public static IEnumerable<TTSource> Add<TTSource>(this IEnumerable<TTSource> source, Func<TTSource> item)
+        {
+            return source
+                .Safe()
+                .Concat(item.Invoke().ToEnumerable());
+        }
+    }
+}
