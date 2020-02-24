884486e13841fe3d4b3f8bfbac32f3ba1dab7aac
Manuel Mayer <manuel.mayer@acamti.ca>
2020-02-23 19:40:34 -0500

    Add AsIndex Extension

---
+++ src/Be.Fluent/With/IEnumerables/AsIndexExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Acamti.Be.Fluent.With.IEnumerables
+{
+    public static class AsIndexExtensions
+    {
+        public static IEnumerable<KeyValuePair<int, TSource>> AsIndex<TSource>(this IEnumerable<TSource> source)
+        {
+            return source?.Select(
+                (item, index) =>
+                    new KeyValuePair<int, TSource>(index, item)
+            );
+        }
+    }
+}
