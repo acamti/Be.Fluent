f44520771859630da086ba7c8b9ea1c6ddf2b5f8
Manuel Mayer <manuel.mayer@acamti.ca>
2020-02-27 09:27:40 -0500

    ToBuilder Extension added

---
+++ src/Be.Fluent.Tests/Describe_With_Generics/Describe_ToBuilderExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Acamti.Be.Fluent.Builders.Generic;
+using Acamti.Be.Fluent.With.Generics;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Be.Fluent.Tests.Describe_With_Generics
+{
+    [TestClass]
+    public class Describe_ToBuilderExtensions
+    {
+        [TestMethod]
+        public void It_ShouldCreateBuilderOutOfModel()
+        {
+            var myClass = new TestHelperClass
+            {
+                Prop1 = "some value",
+                Prop2 = 54,
+                Prop3 = true
+            };
+
+            var expected = new TestHelperClass
+            {
+                Prop1 = "some value",
+                Prop2 = 54,
+                Prop3 = true
+            };
+
+            GenericBuilder<TestHelperClass> result = myClass.ToBuilder();
+
+            result.Build().Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void It_ShouldCreateEmptyBuilderOutOfModel()
+        {
+            var myClass = new TestHelperClass
+            {
+                Prop1 = "some value",
+                Prop2 = 54,
+                Prop3 = true
+            };
+
+            var expected = new TestHelperClass();
+
+            GenericBuilder<TestHelperClass> result = myClass.ToEmptyBuilder();
+
+            result.Build().Should().BeEquivalentTo(expected);
+        }
+
+        private class TestHelperClass
+        {
+            public string Prop1 { get; set; }
+            public int Prop2 { get; set; }
+            public bool Prop3 { get; set; }
+        }
+    }
+}
---
+++ src/Be.Fluent/With/Generics/ToBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Acamti.Be.Fluent.Builders.Generic;
+
+namespace Acamti.Be.Fluent.With.Generics
+{
+    public static class ToBuilderExtensions
+    {
+        public static GenericBuilder<T> ToBuilder<T>(this T source)
+            where T : class =>
+            new GenericBuilder<T>(source);
+
+        public static GenericBuilder<T> ToEmptyBuilder<T>(this T source)
+            where T : class, new() =>
+            new GenericBuilder<T>(new T());
+    }
+}
