b9f87aae9d9b9581e930129b239b9efbe8f2ffde
Manuel Mayer <manuel.mayer@acamti.ca>
2020-03-26 17:32:49 -0400

    reordered

--- src/Be.Fluent/With/Generics/ActExtensions.cs
+++ src/Be.Fluent/With/Generics/ActExtensions.cs
@@ -41,15 +41,6 @@ namespace Acamti.Be.Fluent.With.Generics
             return source;
         }
 
-        public static async Task<TSource> ActAsync<TSource>(this Task<TSource> source, Action<TSource> action)
-        {
-            TSource immutableSource = await source.CloneAsync();
-
-            immutableSource.Act(action);
-
-            return await source;
-        }
-
         public static async Task<TSource> ActAsync<TSource>(this TSource source, Func<TSource, Task> action)
         {
             TSource immutableSource = source.Clone();
@@ -68,15 +59,6 @@ namespace Acamti.Be.Fluent.With.Generics
             return source;
         }
 
-        public static async Task<TSource> ActAsync<TSource, TParam1>(this Task<TSource> source, Action<TSource, TParam1> action, TParam1 p1)
-        {
-            TSource immutableSource = await source.CloneAsync();
-
-            immutableSource.Act(action, p1);
-
-            return await source;
-        }
-
         public static async Task<TSource> ActAsync<TSource, TParam1, TParam2>(this TSource source, Func<TSource, TParam1, TParam2, Task> action, TParam1 p1, TParam2 p2)
         {
             TSource immutableSource = source.Clone();
@@ -86,15 +68,6 @@ namespace Acamti.Be.Fluent.With.Generics
             return source;
         }
 
-        public static async Task<TSource> ActAsync<TSource, TParam1, TParam2>(this Task<TSource> source, Action<TSource, TParam1, TParam2> action, TParam1 p1, TParam2 p2)
-        {
-            TSource immutableSource = await source.CloneAsync();
-
-            immutableSource.Act(action, p1, p2);
-
-            return await source;
-        }
-
         public static async Task<TSource> ActAsync<TSource, TParam1, TParam2, TParam3>(this TSource source, Func<TSource, TParam1, TParam2, TParam3, Task> action, TParam1 p1, TParam2 p2, TParam3 p3)
         {
             TSource immutableSource = source.Clone();
@@ -102,6 +75,33 @@ namespace Acamti.Be.Fluent.With.Generics
             await action(immutableSource, p1, p2, p3);
 
             return source;
+        }
+
+        public static async Task<TSource> ActAsync<TSource>(this Task<TSource> source, Action<TSource> action)
+        {
+            TSource immutableSource = await source.CloneAsync();
+
+            immutableSource.Act(action);
+
+            return await source;
+        }
+
+        public static async Task<TSource> ActAsync<TSource, TParam1>(this Task<TSource> source, Action<TSource, TParam1> action, TParam1 p1)
+        {
+            TSource immutableSource = await source.CloneAsync();
+
+            immutableSource.Act(action, p1);
+
+            return await source;
+        }
+
+        public static async Task<TSource> ActAsync<TSource, TParam1, TParam2>(this Task<TSource> source, Action<TSource, TParam1, TParam2> action, TParam1 p1, TParam2 p2)
+        {
+            TSource immutableSource = await source.CloneAsync();
+
+            immutableSource.Act(action, p1, p2);
+
+            return await source;
         }
 
         public static async Task<TSource> ActAsync<TSource, TParam1, TParam2, TParam3>(this Task<TSource> source, Action<TSource, TParam1, TParam2, TParam3> action, TParam1 p1, TParam2 p2, TParam3 p3)
