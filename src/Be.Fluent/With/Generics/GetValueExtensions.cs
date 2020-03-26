525ce94eb606aea6a9559ac36fcfa4d81ebd2491
Manuel Mayer <manuel.mayer@acamti.ca>
2020-03-26 17:12:48 -0400

    code formating

--- src/Be.Fluent/With/Generics/GetValueExtensions.cs
+++ src/Be.Fluent/With/Generics/GetValueExtensions.cs
@@ -6,11 +6,13 @@ namespace Acamti.Be.Fluent.With.Generics
 {
     public static class GetValueExtensions
     {
-        public static async Task<TProp> GetValueAsync<TModel, TProp>(this Task<TModel> source, Expression<Func<TModel, TProp>> prop) =>
+        public static async Task<TProp> GetValueAsync<TModel, TProp>(this Task<TModel> source,
+                                                                     Expression<Func<TModel, TProp>> prop) =>
             prop.Compile()
                 .Invoke(await source);
 
-        public static async Task<TProp> GetValueAsync<TModel, TProp>(this Func<Task<TModel>> source, Expression<Func<TModel, TProp>> prop) =>
+        public static async Task<TProp> GetValueAsync<TModel, TProp>(this Func<Task<TModel>> source,
+                                                                     Expression<Func<TModel, TProp>> prop) =>
             prop.Compile()
                 .Invoke(await source());
     }
