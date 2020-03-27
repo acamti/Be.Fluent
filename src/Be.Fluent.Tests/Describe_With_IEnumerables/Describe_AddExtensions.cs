2ee7ffb95a8b33db9a59d1ec1c7ed7550fc377eb
Manuel Mayer <manuel.mayer@acamti.ca>
2020-03-26 21:04:24 -0400

    added new add extensions

--- src/Be.Fluent.Tests/Describe_With_IEnumerables/Describe_AddExtensions.cs
+++ src/Be.Fluent.Tests/Describe_With_IEnumerables/Describe_AddExtensions.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Acamti.Be.Fluent.With.IEnumerables;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -58,25 +59,49 @@ namespace Be.Fluent.Tests.Describe_With_IEnumerables
         }
 
         [TestMethod]
-        public void It_Should_ReturnIEnumerable_WithAddedItem_WhenSourceIsNull()
+        public async Task It_Should_ReturnIEnumerable_WithAddedAsyncItem()
         {
             IEnumerable<string> expected = new[] { "newItem" };
 
-            IEnumerable<string> initialList = null;
+            Task<IEnumerable<string>> initialList = Task.FromResult(Enumerable.Empty<string>());
 
-            IEnumerable<string> result = initialList.Add("newItem");
+            IEnumerable<string> result = await initialList.AddAsync("newItem");
 
             result.Should().BeEquivalentTo(expected);
         }
 
         [TestMethod]
-        public void It_Should_ReturnIEnumerable_WithAddedItemFromPredicate_WhenSourceIsNull()
+        public async Task It_Should_ReturnIEnumerable_WithAddedAsyncItem_WhenNotEmpty()
+        {
+            IEnumerable<string> expected = new[] { "oldItem", "newItem" };
+
+            Task<IEnumerable<string>> initialList = Task.FromResult(new[] { "oldItem" }.AsEnumerable());
+
+            IEnumerable<string> result = await initialList.AddAsync("newItem");
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public async Task It_Should_ReturnIEnumerable_WithAddedAsyncItemFromPredicate()
         {
             IEnumerable<string> expected = new[] { "newItem" };
 
-            IEnumerable<string> initialList = null;
+            Task<IEnumerable<string>> initialList = Task.FromResult(Enumerable.Empty<string>());
 
-            IEnumerable<string> result = initialList.Add(() => "newItem");
+            IEnumerable<string> result = await initialList.AddAsync(() => "newItem");
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public async Task It_Should_ReturnIEnumerable_WithAddedAsyncItemFromPredicate_WhenNotEmpty()
+        {
+            IEnumerable<string> expected = new[] { "oldItem", "newItem" };
+
+            Task<IEnumerable<string>> initialList = Task.FromResult(new[] { "oldItem" }.AsEnumerable());
+
+            IEnumerable<string> result = await initialList.AddAsync(() => "newItem");
 
             result.Should().BeEquivalentTo(expected);
         }
--- src/Be.Fluent/With/IEnumerables/AddExtensions.cs
+++ src/Be.Fluent/With/IEnumerables/AddExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Acamti.Be.Fluent.With.Generics;
 
 namespace Acamti.Be.Fluent.With.IEnumerables
@@ -8,13 +9,15 @@ namespace Acamti.Be.Fluent.With.IEnumerables
     public static class AddExtensions
     {
         public static IEnumerable<TTSource> Add<TTSource>(this IEnumerable<TTSource> source, TTSource item) =>
-            source
-                .Safe()
-                .Concat(item.ToEnumerable());
+            source.Concat(item.ToEnumerable());
 
         public static IEnumerable<TTSource> Add<TTSource>(this IEnumerable<TTSource> source, Func<TTSource> funcOfItem) =>
-            source
-                .Safe()
-                .Concat(funcOfItem().ToEnumerable());
+            source.Concat(funcOfItem().ToEnumerable());
+
+        public static async Task<IEnumerable<TTSource>> AddAsync<TTSource>(this Task<IEnumerable<TTSource>> source, TTSource item) =>
+            (await source).Concat(item.ToEnumerable());
+
+        public static async Task<IEnumerable<TTSource>> AddAsync<TTSource>(this Task<IEnumerable<TTSource>> source, Func<TTSource> funcOfItem) =>
+            (await source).Concat(funcOfItem().ToEnumerable());
     }
 }
