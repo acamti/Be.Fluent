8e1e9d7fdfafee7f6826f683893b6b8dc8b48ef7
Manuel Mayer <manuel.mayer@acamti.ca>
2020-03-26 22:01:48 -0400

    rename GetValue to WithValue

---
+++ src/Be.Fluent/With/Generics/WithValueExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Acamti.Be.Fluent.With.Generics
+{
+    public static class WithValueExtensions
+    {
+        public static async Task<TProp> WithValueAsync<TModel, TProp>(this Task<TModel> source,
+                                                                      Expression<Func<TModel, TProp>> prop) =>
+            prop.Compile()(await source);
+
+        public static async Task<TProp> WithValueAsync<TModel, TProp>(this Func<Task<TModel>> source,
+                                                                      Expression<Func<TModel, TProp>> prop) =>
+            prop.Compile()(await source());
+    }
+}
